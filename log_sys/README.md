e7052dbe40669def7d4ae4c0fd416f9bb8fc22fd
Cade Wang <47269953+valourshadow@users.noreply.github.com>
2022-07-20 17:00:28 +0800

    log  sys v1.0  by wwt

---
+++ log_sys/README.md
@@ -0,0 +1,9 @@
+# Log
+This is a log system implemented in C++.
+
+# Compile
+mingw-w64
+
+# v1.0
+source code 
+little problem
---
+++ log_sys/build/CMakeFiles/LOG_SYS.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LOG_SYS.dir/src/log.cpp.obj"
+  "CMakeFiles/LOG_SYS.dir/src/log.cpp.obj.d"
+  "CMakeFiles/LOG_SYS.dir/test/test.cpp.obj"
+  "CMakeFiles/LOG_SYS.dir/test/test.cpp.obj.d"
+  "LOG_SYS.exe"
+  "LOG_SYS.exe.manifest"
+  "LOG_SYS.pdb"
+  "libLOG_SYS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LOG_SYS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
